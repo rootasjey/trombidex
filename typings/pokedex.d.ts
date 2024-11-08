608d69fb4dc6ffb8405d5918a0a2172e687c2cc8
rootasjey <5097282+rootasjey@users.noreply.github.com>
2024-11-09 00:43:52 +0100

    feat: :label: update type definitions

--- typings/pokedex.d.ts
+++ typings/pokedex.d.ts
@@ -1,163 +1,53 @@
-interface Ability {
-  name: string;
-  url: string;
+
+type Ability = {
+  ability: {
+    name: string
+  }
+  is_hidden: boolean
+  slot: number
 }
 
-interface AbilityEntry {
-  ability: Ability;
-  is_hidden: boolean;
-  slot: number;
+
+type GameIndice = {
+  game_index: number
+  version: {
+    name: string
+    url: string
+  }
 }
 
-interface AvgStats {
-  [key: string]: number;
-
-  attack: number;
-  defense: number;
-  hp: number;
-  specialAttack: number;
-  specialDefense: number;
-  speed: number;
+type NameLocalized = {
+  language: {
+    name: string
+  }
+  name: string
 }
 
-interface AvgStatsMeta {
-  lastUpdated: string;
+type Pokemon = {
+  abilities: Ability[]
+  game_indices: {
+    game_index: number
+    version: {
+      name: string
+    }
+  }[]
+  height: number
+  id: number
+  name: string
+  sprites: {
+    front_default: string
+  }
+  types: {
+    type: {
+      name: string
+    }
+  }[]
+  weight: number
 }
 
-interface Controversy {
-  dislikes: number;
-  id: number;
-  likes: number;
-  name: string;
-}
-
-interface ControversyResponse {
-  controversy: Controversy;
-}
-
-interface DislikeResponse {
-  dislike: Controversy;
-}
-
-interface LikeResponse {
-  like: Controversy;
-}
-
-interface ListResponse {
-  list: PokestatsList;
-}
-
-interface MinimalPokemon {
-  id: number;
-  isBookmarked: boolean;
-  name: string;
-  sprites: PokestatsSprites;
-  url: string;
-}
-
-interface Pokemon {
-  abilities: AbilityEntry[];
-  id: number;
-  isBookmarked: boolean;
-  name: string;
-  sprites: Sprites;
-  stats: StatEntry[];
-  types: TypeEntry[];
-}
-
-interface PokemonByIdResponse {
-  pokemonById: Pokemon[];
-}
-
-interface PokestatsList {
-  results: MinimalPokemon[];
-}
-
-interface PokestatsSprites {
-  defaultFront: string;
-}
-
-interface PokestatsResponse {
-  averageStats: PokestatsResponseData;
-}
-
-interface PokestatsResponseData {
-  avg: AvgStats;
-  meta: AvgStatsMeta;
-
-  pokemonCount: number;
-
-  types: string[];
-}
-
-interface Sprites {
-  [key: string]: string;
-  femaleBack: string;
-  femaleFront: string;
-  femaleShinyBack: string;
-  femaleShinyFront: string;
-  defaultBack: string;
-  defaultFront: string;
-  defaultShinyBack: string;
-  defaultShinyFront: string;
-}
-
-interface SpritesByIdResponse {
-  spritesByIds: [SpritesByIdResponseData];
-}
-
-interface SpritesByIdResponseData {
-  id: number;
-  name: string;
-  sprites: Sprites;
-}
-
-interface Stat {
-  name: string;
-  url: string;
-}
-
-interface StatEntry {
-  baseStat: number;
-  effort: number;
-  stat: Stat;
-}
-
-interface Type {
-  name: string;
-  url: string;
-}
-
-interface TypeEntry {
-  slot: number;
-  type: Type;
-}
-
-interface Tweet {
-  created_at: string;
-  id_str: string;
-  text: string;
-  user: TweetUser;
-}
-
-interface TweetServiceResponseData {
-  tweets: TweetServiceResponseStatuses;
-}
-
-interface TweetServiceResponseStatuses {
-  statuses: Tweet[];
-}
-
-interface TweetSubscriptionData {
-  tweetAdded: Tweet;
-}
-
-interface TweetSubscriptionResponse {
-  data: TweetSubscriptionData;
-}
-
-interface TweetUser {
-  name: string;
-  profile_image_url: string;
-  screen_name: string;
+type PokemonType = {
+  slot: number
+  type: {
+    name: string
+  }
 }
