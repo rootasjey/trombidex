69726f583dd0390e406c5d537314b5711f70c92c
rootasjey <5097282+rootasjey@users.noreply.github.com>
2024-11-09 20:48:23 +0100

    refactor: :recycle: put query strings extraction into dedicated function

--- server/api/list.ts
+++ server/api/list.ts
@@ -23,14 +23,14 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
+/**
+ * Retrieves a list of pokemons from Firestore, with optional pagination.
+ *
+ * @param event - The event object containing the query parameters.
+ * @returns An array of pokemon objects, with the `types` property containing an array of type names.
+ */
 export default defineCachedEventHandler(async (event) => {
-  let { limit : max = 24, offset = 0 } = getQuery(event)
-  
-  max = typeof max === "string" ? parseInt(max as string) : max
-  offset = typeof offset === "string" ? parseInt(offset as string) : offset
-
-  max = typeof max !== "number" || max < 1 ? 24 : max
-  offset = typeof offset !== "number" || offset < 0 ? 0 : offset
+  const { max, offset } = extractQueryParams(event)
 
   const collectionRef = collection(db, "pokemons")
   const q = query(collectionRef, orderBy("id"), startAt(offset), limit(max))
@@ -44,6 +44,31 @@ export default defineCachedEventHandler(async (event) => {
       types: data.types.map((typeRecord: any) => typeRecord.type.name)
     }
   })
-}, 
-{ maxAge: 3600 * 1, }
+}, { 
+  maxAge: 3600 * 1,
+}
 );
+
+/**
+ * Extracts the `limit` and `offset` query parameters from the provided event object.
+ *
+ * @param event - The event object containing the query parameters.
+ * @returns An object with the `max` and `offset` properties, representing the extracted query parameters.
+ */
+const extractQueryParams = (event: any) => {
+  let { limit: max = 24, offset = 0 } = getQuery(event)
+
+  max = typeof max === "string" ? parseInt(max as string) : max
+  offset = typeof offset === "string" ? parseInt(offset as string) : offset
+
+  max = typeof max !== "number" || max < 1 ? 24 : max
+  offset = typeof offset !== "number" || offset < 0 ? 0 : offset
+
+  max = isNaN(max) ? 24 : max
+  offset = isNaN(offset) ? 0 : offset
+
+  return {
+    max,
+    offset,
+  }
+}
